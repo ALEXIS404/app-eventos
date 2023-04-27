e31fae6f71a1fa7bdbeef9ecd28a7b9b1577eb5c
ALEXIS404 <est.asanchezc@uml.edu.ni>
2023-04-26 20:46:23 -0600

    cambios

--- src/js/paginas/barra.js
+++ src/js/paginas/barra.js
@@ -51,7 +51,7 @@ class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-g
           Calendario
           <div class="tooltip-arrow" data-popper-arrow></div>
       </div>
-      <a data-tooltip-target="tooltip-profile" href="#" class="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
+      <a data-tooltip-target="tooltip-profile" href="perfil.html" class="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
           <svg class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path clip-rule="evenodd" fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"></path>
           </svg>
