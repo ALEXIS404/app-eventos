d0d6c1032918c3e77cab7afecd262d5d3e4607a2
ALEXIS404 <est.asanchezc@uml.edi.ni>
2023-05-11 21:54:44 -0600

    cambios js

--- src/js/modelos/artistas.js
+++ src/js/modelos/artistas.js
@@ -1,9 +1,22 @@
-//importar fire
-import '../firebase';
+//Importar firebase y su configuracion
+import {db} from '../firebase';
 
-import { getFirestore, collection } from 'firebase/firestore';
+//Importar getFirestore para leer la base de datos
+import { collection, query, getDocs} from 'firebase/firestore';
 
-const db = getFirestore(app);
+//Leer la coleccion de artistas
+const artistas = query(collection(db, "artistas"));
 
-const artistas = collection(db, 'artistas');
+//Leer los documentos de la coleccion de forma asincrona
+const querySnapshot = await getDocs(artistas);
 
+let artistas_contenedor = document.getElementById('artistas');
+
+querySnapshot.forEach((doc) => {    
+  // doc.data() is never undefined for query doc snapshots
+  let artista = doc.data();
+  artistas_contenedor.innerHTML += `<li>${artista.nombres} - ${artista.nacionalidad} 
+  <img src="${artista.imagen}" alt=${artista.nombres}" class="w-48 h-48 rounded-full">
+   
+  </li>`;
+});
